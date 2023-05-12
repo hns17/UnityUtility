6700e2fe406df6d1e58e34279d89a3f43c1d48a9
hns17 <hns17@naver.com>
2023-05-12 15:39:33 +0900

    no message

---
+++ Assets/Utility/Editor/GraphicRaycastTargetToggle.cs
@@ -0,0 +1,39 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+/**
+ *  @brief  : Grphic Component의 RaycastTarget 설정이 가능한 토글박스를 Hierachy에 표시.
+ *  @ref    : https://baba-s.hatenablog.com/entry/2019/04/01/092000
+ */
+public static class GraphicRaycastTargetToggle
+{
+    private const int WIDTH = 16;
+
+    [InitializeOnLoadMethod]
+    private static void RayTargetView()
+    {
+        EditorApplication.hierarchyWindowItemOnGUI += OnGUI;
+    }
+
+    private static void OnGUI(int instanceID, Rect selectionRect)
+    {
+        var go = EditorUtility.InstanceIDToObject(instanceID) as GameObject;
+
+        if (go == null) return;
+
+        var com = go.GetComponent<Graphic>();
+
+        if (com == null) return;
+
+        var pos = selectionRect;
+        pos.x = pos.xMax - WIDTH;
+        pos.width = WIDTH;
+
+        var raycastTarget = GUI.Toggle(pos, com.raycastTarget, string.Empty);
+
+        if (raycastTarget == com.raycastTarget) return;
+
+        com.raycastTarget = raycastTarget;
+    }
+}
---
+++ Assets/Utility/Runtime/ProjectManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Resources;
+using UnityEngine;
+
+
+/**
+ *  @brief  Project의 Manager기능을 통합 후 서비스 로케이터로 사용
+ *          Monobehaviour Singletone으로 직접 삭제하지 않는한 삭제되지 않는 유일성을 가진 오브젝트
+ */
+public class ProjectManager : MonoSingleton<ProjectManager>
+{
+    
+}
